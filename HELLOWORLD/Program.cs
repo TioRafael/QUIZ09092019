8a85f1911f4ac67b119f4637c5d752e907e86ae0
TioRafael <54490069+TioRafael@users.noreply.github.com>
2019-09-11 20:52:31 +0700

    Add files via upload

---
+++ HELLOWORLD/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+    namespace QUIZ09092019
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                BangunDatar obj = new BangunDatar();
+
+                obj.luasPersegi();
+
+                obj.luasSegitiga();
+
+                obj.luasLingkaran();
+            }
+        }
+    }
